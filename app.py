e88c1530f665175c394b4f1cd6def8c2a9fc07cc
Supratim Saha <supratimsmail@gmail.com>
2025-12-06 17:52:04 +0530

    Updated files for deloyment

--- app.py
+++ app.py
@@ -57,5 +57,5 @@ def index():
 
     return render_template('index.html', prediction=prediction, probability=probability)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
