8d0a80c5731ace62dd45a51df33c15406a003bf7
Supratim Saha <supratimsmail@gmail.com>
2025-12-06 17:56:47 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import pandas as pd
 import joblib
+import os
 
 # Load model and scaler
 X_train = joblib.load("X_train_columns.pkl")
@@ -58,4 +59,5 @@ def index():
     return render_template('index.html', prediction=prediction, probability=probability)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
